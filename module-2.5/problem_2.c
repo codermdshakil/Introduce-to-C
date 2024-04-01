65ef69aad99121cb31fffcca0325ff3b5574d7e9
codermdshakil <ahmedshakil0512@gmail.com>
2024-04-02 00:04:50 +0600

    Problem two added

--- module-2.5/problem_2.c
+++ module-2.5/problem_2.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 
     int a, b;
     scanf("%d %d", &a, &b);
-    printf("%d \n", a);    
-    printf("%d", b);    
+    int sum = a + b;
+    printf("%d + %d = %d \n", a, b, sum);
+
+    int sub = a - b;
+    printf("%d - %d = %d \n", a, b, sub);
+
+    int mul = a * b;
+    printf("%d * %d = %d \n", a, b, mul);
+
+    int div = a / b;
+    printf("%d / %d = %d \n", a, b, div);
 
     return 0;
 }
