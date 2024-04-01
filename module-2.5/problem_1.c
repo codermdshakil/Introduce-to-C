46fbe301a804d28987a7004d1e84b85ad82b6192
codermdshakil <ahmedshakil0512@gmail.com>
2024-04-01 22:10:17 +0600

    problem 1 added

---
+++ module-2.5/problem_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+
+
+    printf("\" Recently I heard that you\'ve achieved 95  marks in your Exam!");
+    printf("I wish you\'ll shine in your life! \t Good luck will the barriers (/\\) in your life. \" ");
+    return 0;
+}
+
