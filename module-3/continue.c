748933d6d71a54d7987bc9cb91b135ae6941c7d5
codermdshakil <ahmedshakil0512@gmail.com>
2024-04-03 14:27:17 +0600

    Continue

---
+++ module-3/continue.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // Skip specific number
+    for (int i = 1; i <= 10; i++)
+    {
+        if (i == 8)
+        {
+            continue;
+        }
+        printf("%d\n", i);
+    }
+
+    // find odd number
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     if (i%2 == 0)
+    //     {
+    //         continue;
+    //     }
+    //     printf("%d\n", i);
+    // }
+
+    // Find Even number
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     if (i % 2 > 0)
+    //     {
+    //         continue;
+    //     }
+    //     printf("%d\n", i);
+    // }
+
+    return 0;
+}
+
+/**
+- continue - যদি রাস্তায় সামনে কোণ বিপদ থাকে তাহলে আমরা কি করি লাপ দিয়ে পার হই ঠিক তেমনি continue ও কাজ করে । লুপ এর মধ্যে যখন যদি condition এ বলা থাকে যে আইখানে থাম তারপর থেকে চালাও এই কাজটি loop এর মধ্যে continue করে ।
+- continue - এর নিছের লাইন কে skip করে।
+*/
