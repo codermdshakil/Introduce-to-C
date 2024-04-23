288ef594e6db5f8779d354d844d2b3abba139c0f
codermdshakil <ahmedshakil0512@gmail.com>
2024-04-23 19:00:53 +0600

    Problem 2 is added

--- module-8-assignment-2/problem_2.c
+++ module-8-assignment-2/problem_2.c
@@ -14,13 +14,16 @@ int main()
                 printf("%d ", i);
             }
         }
+        printf("\n");
     }
-    else
+    else if (n < 0)
     {
-        for (int i = 0; i >= n; i--)
+        for (int i = n; i <= 0; i++)
         {
             printf("%d ", i);
         }
+
+        printf("\n");
     }
 
     return 0;
