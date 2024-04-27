87448542aaf8b4227b5ba14e8dd7fc05335cfa3e
codermdshakil <ahmedshakil0512@gmail.com>
2024-04-27 11:24:25 +0600

    module-1 added

---
+++ week-1-orientation/module-1/hello.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    // int number = 50;
+    // float n1 = 5.4456;
+    // char names = 'S';
+    // printf("%d", number);
+    // printf("%0.2f", n1);
+
+// take input 
+// int rahim, karim;
+// scanf("%d %d", &rahim, &karim);
+// printf("%d %d", rahim, karim);
+// & means address of 
+
+// int n;
+// float f;
+// char c;
+
+// scanf("%d %f %c", &n ,&f ,&c);
+// printf("%d %0.2f %c", n, f,c );
+
+
+int a, b;
+char p;
+
+// scanf(" %d%c %d%c", &a, &p,&b, &p );
+// printf("%d%% %d%%" , a, b);
+
+scanf(" %d%% %d%%", &a, &b );
+printf("%d%% %d%%" , a, b);
+    return 0;
+
+}
