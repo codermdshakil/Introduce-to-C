b1edef7d42960048ef0e2763cd9e564c021622fc
codermdshakil <ahmedshakil0512@gmail.com>
2024-05-06 20:20:00 +0600

    module 7 practice day added

---
+++ week-2-loop recap and array/module-7/array_init.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    // array init with value
+    int ar[5] = {11,22,33,44,55};
+
+    for (int i = 0; i <5 ; i++)
+    {
+        printf("%d\n", ar[i]);
+    }
+    
+
+    return 0;
+}
---
+++ week-2-loop recap and array/module-7/array_input_output.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    // take input and print it 
+
+    // int n;
+    // scanf("%d", &n);
+    // int ar[1];
+    // ar[0] = n;
+    // ar[1] = n + 1;
+
+    // printf("%d\n", ar[0]);
+    // printf("%d\n", ar[1]);
+
+    // int ar[3];
+    // ar[0] = 100;
+    // ar[1] = 200;
+    // ar[2] = 300;
+    // printf("%d %d %d", ar[0],ar[1],ar[2]);
+    
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+       printf("ar[%d]\n", i);
+    }
+    
+
+
+
+    return 0;
+}
