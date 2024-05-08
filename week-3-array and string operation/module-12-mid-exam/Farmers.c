3aa532210c7801b9d0c1e2f02ca62fe526e4833a
codermdshakil <ahmedshakil0512@gmail.com>
2024-05-08 21:20:56 +0600

    framers problem solved

--- week-3-array and string operation/module-12-mid-exam/Farmers.c
+++ week-3-array and string operation/module-12-mid-exam/Farmers.c
@@ -1,14 +1,22 @@
+
 #include <stdio.h>
 
 int main()
 {
-    int m1, m2,d;
-    scanf("%d %d %d", &m1, &m2, &d);
+    int t;
 
-    
+    scanf("%d", &t);
 
+    for (int i = 0; i < t; i++)
+    {
+        int m1, m2, d;
+        scanf("%d %d %d", &m1, &m2, &d);
 
+        float work_rate = m1 / d;
+        float reduce_day = (float)m1 / (m1 + m2) * d;
+        int fewer_days = d - (int)reduce_day;
+        printf("%d \n", fewer_days);
+    }
 
-    
     return 0;
 }
