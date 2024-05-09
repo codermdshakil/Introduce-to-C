6345b49a2596cfaa49aef44a3ffab3fe7b5dd1d7
codermdshakil <ahmedshakil0512@gmail.com>
2024-05-10 00:26:28 +0600

    task 3 done

--- week-4-nested-loop-recap-function-pointer/module-13/task_3.c
+++ week-4-nested-loop-recap-function-pointer/module-13/task_3.c
@@ -4,19 +4,20 @@ int main()
 {
     int n, k;
     scanf("%d", &n);
-    k = n;
+    k=5;
 
     for (int i = 1; i <= n; i++)
     {
-        // dec
-        for (int j = k; j >= 1; j--)
+        for (int j = k; j >= 1 ; j--)
         {
             printf("%d ", j);
         }
         printf("\n");
         k--;
-        
     }
+
+
     
+
     return 0;
 }
