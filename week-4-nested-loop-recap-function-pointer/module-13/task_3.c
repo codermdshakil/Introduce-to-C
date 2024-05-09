b1c264958f2bd8472dfe07fc6968eaf7da1b3c85
codermdshakil <ahmedshakil0512@gmail.com>
2024-05-10 00:05:52 +0600

    n to 1 print using nested loop

--- week-4-nested-loop-recap-function-pointer/module-13/task_3.c
+++ week-4-nested-loop-recap-function-pointer/module-13/task_3.c
@@ -2,8 +2,21 @@
 
 int main()
 {
-    int n;
+    int n, k;
     scanf("%d", &n);
+    k = n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        // dec
+        for (int j = k; j >= 1; j--)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+        k--;
+        
+    }
     
     return 0;
 }
