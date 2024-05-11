f97d17d2476b497d5ef32416356d4a1b99c5ef27
codermdshakil <ahmedshakil0512@gmail.com>
2024-05-12 01:28:23 +0600

    build in func of math.h header file like ceil, floor, round, sqrt, pow,abs

---
+++ week-4-nested-loop-recap-function-pointer/module-14/build_int_func.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float n = 3.1;
+    // int rs = ceil(n); // 4
+    // int rs = floor(n); // 3
+    // int rs = round(n); // 3 integer value
+    // int rs = sqrt(16); // returns square root of 16 is 4
+    int rs = pow(4,3); // 4*4*4 = 64
+    printf("%d", rs);
+    return 0;
+}
---
+++ week-4-nested-loop-recap-function-pointer/module-14/math_func.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    // double x;
+    // scanf("%lf", &x);
+    // int ans = ceil(x); // 3.001 -> 4
+    // int ans = floor(x); // 3.9 -> 3 
+    // int ans = round(x); 3.4 -> 3, 3.5-> 4
+    // double ans = sqrt(21); // 4.58
+    // printf("%.2lf", ans);
+    // int ans = pow(2,3); // 8
+
+    // double a, b;
+    // scanf("%lf %lf", &a, &b);
+    // double ans = pow(a, b);
+    // printf("%.2lf", ans);
+
+    int ans = abs(-5); // 5
+    printf("%d", ans);
+
+    return 0;
+}
