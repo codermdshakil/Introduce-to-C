837ee3f851233cc3f1b61f1860a1193b31dd864e
codermdshakil <ahmedshakil0512@gmail.com>
2024-05-14 00:45:55 +0600

    pointer 2 explain

---
+++ week-4-nested-loop-recap-function-pointer/module-15/pointer_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    double x = 5.26;
+    double * ptr = &x;
+    double *ptr2 = ptr;
+
+    *ptr = 10.00;
+    *ptr2 = 15.4;
+    printf("x er value - %0.2lf\n", x);
+    printf("x er value - %0.2lf\n", *ptr);
+    printf("x er value - %0.2lf\n", *ptr2);
+
+    return 0;
+}
