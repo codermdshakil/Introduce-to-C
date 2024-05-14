90ab76d2a17fc8312b7e0c9a1e938f484dd59d71
codermdshakil <ahmedshakil0512@gmail.com>
2024-05-14 16:09:04 +0600

    pass by reference

---
+++ week-4-nested-loop-recap-function-pointer/module-15/pass_value_fun.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void fun(int x){
+    printf("fun - %d\n", x); // 10
+}
+int main()
+{
+    int x = 10;
+    fun(x);
+    x = 20;
+    printf("M - %d\n", x);
+    return 0;
+}
