234772aecb9f06e728d0966e7f64c281c42d1094
codermdshakil <ahmedshakil0512@gmail.com>
2024-05-20 13:51:50 +0600

    palindrome using while loop

---
+++ week-5-2D-array/module-19/palindrome.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char a[1001], b[1001];
+    scanf("%s", &a);
+    strcpy(b, a); // b er modhe a er ta copy koro
+
+    // swap 
+    int i = 0, j=strlen(b)-1;
+    while (i<j)
+    {
+        char tmp = b[i];
+        b[i]=b[j];
+        b[j] = tmp;
+        i++;
+        j--;
+    }
+
+    if (strcmp(a, b) == 0)
+    {
+        printf("YES\n");
+    }
+    else{
+        
+        printf("NO\n");
+    }
+    
+    
+    printf("%s", b);
+
+    return 0;
+}
