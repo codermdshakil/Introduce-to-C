3457ed92f5fba263fd87155c0ec6610c20565321
codermdshakil <ahmedshakil0512@gmail.com>
2024-05-20 13:58:15 +0600

    palindrome using two pointer

---
+++ week-5-2D-array/module-19/palindrome_two_pointer.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char a[1001];
+    scanf("%s", a);
+    int i = 0, j= strlen(a)-1;
+
+    int flag = 1;
+    while (i < j)
+    {
+
+        // using two pointer 
+        if (a[i] != a[j])
+        {
+           flag = 0;
+        }
+        
+        i++;
+        j--;
+    }
+    
+    if (flag == 0)
+    {
+        printf("NO\n");
+    }
+    else{
+        
+        printf("YES\n");
+    }
+    
+    return 0;
+}
